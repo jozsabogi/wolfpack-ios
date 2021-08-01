17e01eddc91d49090c599788e55e88cebc7937bb
Jozsa Bogi <jozsabogi@yahoo.com>
2021-08-01 23:07:23 +0300

    protocols

--- Student2/1 - Tables and Persistence/1 - Protocols/Lab - Protocols.playground/Pages/1. Exercise - Adopt Protocols.xcplaygroundpage/Contents.swift
+++ Student2/1 - Tables and Persistence/1 - Protocols/Lab - Protocols.playground/Pages/1. Exercise - Adopt Protocols.xcplaygroundpage/Contents.swift
@@ -3,19 +3,61 @@
  
  Create a `Human` class with two properties: `name` of type `String`, and `age` of type `Int`. You'll need to create a memberwise initializer for the class. Initialize two `Human` instances.
  */
-
+class Human: CustomStringConvertible, Equatable, Comparable, Codable {
+    var name: String
+    var age: Int
+    
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+    
+    var description: String {
+        return "\(name) - \(age)"
+    }
+    
+    static func ==(lhs: Human, rhs: Human) -> Bool {
+        return lhs.name == rhs.name && lhs.age == rhs.age
+    }
+    
+    static func <(lhs: Human, rhs: Human) -> Bool {
+        return lhs.age < rhs.age
+    }
+}
 
 //:  Make the `Human` class adopt the `CustomStringConvertible` protocol. Print both of your previously initialized `Human` objects.
+let human1 = Human(name: "Bogi", age: 22)
+let human2 = Human(name: "Eszter", age: 19)
 
+print(human1)
+print(human2)
 
 //:  Make the `Human` class adopt the `Equatable` protocol. Two instances of `Human` should be considered equal if their names and ages are identical to one another. Print the result of a boolean expression evaluating whether or not your two previously initialized `Human` objects are equal to eachother (using `==`). Then print the result of a boolean expression evaluating whether or not your two previously initialized `Human` objects are not equal to eachother (using `!=`).
-
-
+print(human1 != human2)
 //:  Make the `Human` class adopt the `Comparable` protocol. Sorting should be based on age. Create another three instances of a `Human`, then create an array called `people` of type `[Human]` with all of the `Human` objects that you have initialized. Create a new array called `sortedPeople` of type `[Human]` that is the `people` array sorted by age.
+let human3 = Human(name: "Szabolcs", age: 18)
+let human4 = Human(name: "Csenge", age: 17)
+let human5 = Human(name: "Hunor", age: 17)
 
+let people: [Human] = [human1, human2, human3, human4, human5]
 
+let sortedPeople = people.sorted(by: <)
+
+print("Sorted: --------")
+for human in sortedPeople {
+    print(human)
+}
+
+print("--------")
 //:  Make the `Human` class adopt the `Codable` protocol. Create a `JSONEncoder` and use it to encode as data one of the `Human` objects you have initialized. Then use that `Data` object to initialize a `String` representing the data that is stored, and print it to the console.
+import Foundation
 
+let jsonEncoder = JSONEncoder()
+
+if let jsonData = try? jsonEncoder.encode(human1),
+   let jsonString = String(data: jsonData, encoding: .utf8) {
+    print(jsonString)
+}
 
 /*:
 page 1 of 5  |  [Next: App Exercise - Printable Workouts](@next)
--- Student2/1 - Tables and Persistence/1 - Protocols/Lab - Protocols.playground/Pages/5. App Exercise - Heart Rate Delegate.xcplaygroundpage/Contents.swift
+++ Student2/1 - Tables and Persistence/1 - Protocols/Lab - Protocols.playground/Pages/5. App Exercise - Heart Rate Delegate.xcplaygroundpage/Contents.swift
@@ -8,12 +8,18 @@ import UIKit
 
  `HeartRateViewController` below is a view controller that will present the heart rate information to the user. Throughout the exercises below you'll use the delegate pattern to pass information from an instance of `HeartRateReceiver` to the view controller so that anytime new information is obtained it is presented to the user.
  */
+protocol HeartRateReceiverDelegate {
+    func heartRateUpdated(to bpm: Int)
+}
 
 class HeartRateReceiver {
+    var delegate: HeartRateReceiverDelegate?
+    
     var currentHR: Int? {
         didSet {
             if let currentHR = currentHR {
                 print("The most recent heart rate reading is \(currentHR).")
+                delegate?.heartRateUpdated(to: currentHR)
             } else {
                 print("Looks like we can't pick up a heart rate.")
             }
@@ -21,7 +27,7 @@ class HeartRateReceiver {
     }
 
     func startHeartRateMonitoringExample() {
-        for _ in 1...10 {
+        for _ in 1...5 {
             let randomHR = 60 + Int(arc4random_uniform(UInt32(15)))
             currentHR = randomHR
             Thread.sleep(forTimeInterval: 2)
@@ -29,12 +35,23 @@ class HeartRateReceiver {
     }
 }
 
-class HeartRateViewController: UIViewController {
+class HeartRateViewController: UIViewController, HeartRateReceiverDelegate {
     var heartRateLabel: UILabel = UILabel()
+    
+    func heartRateUpdated(to bpm: Int) {
+        heartRateLabel.text = "The user has been shown a heart rate of \(bpm)."
+        if let text = heartRateLabel.text {
+            print(text)
+        }
+    }
 }
 //:  First, create an instance of `HeartRateReceiver` and call `startHeartRateMonitoringExample`. Notice that every two seconds `currentHR` get set and prints the new heart rate reading to the console.
+let HRR = HeartRateReceiver()
 
+let HRVC = HeartRateViewController()
+HRR.delegate = HRVC
 
+HRR.startHeartRateMonitoringExample()
 /*:
  In a real app, printing to the console does not show information to the user. You need a way of passing information from the `HeartRateReceiver` to the `HeartRateViewController`. To do this, create a protocol called `HeartRateReceiverDelegate` that requires a method `heartRateUpdated(to bpm:)` where `bpm` is of type `Int` and represents the new rate as _beats per minute_. Since playgrounds read from top to bottom and the two previously declared classes will need to use this protocol, you'll need to declare this protocol above the declaration of `HeartRateReceiver`.
 
